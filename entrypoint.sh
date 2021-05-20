9d72f3537a81e56623f353777d33fc3eecc39379
Khemarato Bhikkhu <khemarato.bhikkhu@gmail.com>
2021-05-20 13:28:33 +0700

    moving on

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,16 @@ if ! command -v uconv; then
     echo "Please install the uconv command"
     exit 1
 else
-    echo "Got the uconv command"
     echo "Available transliterators are:"
     uconv -L
 fi
+
+IFS=$'\n'
+for file in `git diff-tree --no-commit-id --name-only -r HEAD`; do
+    if [ -f $file ]; then
+        echo "Found \"$file\""
+    else
+        echo "Looked for but couldn't find \"$file\". Perhaps it was removed by that commit?"
+    fi
+done
+
