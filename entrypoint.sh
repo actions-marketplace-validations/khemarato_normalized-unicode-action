3d52a79201789a173a61741f36ca22f7c430b9e5
Khemarato Bhikkhu <khemarato.bhikkhu@gmail.com>
2021-05-20 14:27:19 +0700

    moving along

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-echo "Got arg \"$1\""
+echo "Using transliterator \"$1\""
 
 if ! command -v uconv; then
     echo "Please install the uconv command"
     exit 1
-else
-    echo "Available transliterators are:"
-    uconv -L
+fi
+TRANSLITS=`uconv -L`
+if [[ ${TRANSLITS} != *" $1 "* ]]; then
+    echo "But \"$1\" is not a valid transliterator."
+    echo "Available transliterators are: $TRANSLITS"
+    exit 1
 fi
 
 cd $GITHUB_WORKSPACE
 
-IFS=$'\n'
-foundsomething=false
 TOUCHED_FILES=`git diff-tree --no-commit-id --name-only -r HEAD`
-echo "Found touched files: \"$TOUCHED_FILES\""
 if [ -z "$TOUCHED_FILES" ]; then
     echo "Didn't find any files modified by the last commit"
     echo "Perhaps you forgot to set checkout:fetch-depth to 2?"
     exit 1
 fi
 
+modified=false
+IFS=$'\n'
+echo "I hope $HOME != $GITHUB_WORKSPACE..."
 for file in `isutf8 -i $TOUCHED_FILES`; do
-    foundsomething=true
     if [ -f $file ]; then
-        echo "Found UTF8 file \"$file\""
+        echo "Analyzing \"$file\"..."
+        uconv -x "$1" "$file" > $HOME/tmp
+        if cmp --silent $HOME/tmp "$file"; then
+            rm $HOME/tmp
+        else
+            echo "...Modifying \"$file\""
+            mv -f $HOME/tmp "$file"
+            modified=true
+        fi
     else
         echo "Looked for but couldn't find \"$file\". Perhaps it was removed by that commit?"
     fi
